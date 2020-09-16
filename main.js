e388aab7034da0546be19472b9cb7df9f91137c4
William Amaral <williamamaral521@gmail.com>
2020-09-16 10:19:27 -0300

    Alterações Script

--- main.js
+++ main.js
@@ -11,29 +11,5 @@ app.whenReady().then(() => {
 
   newWindow.loadFile("index.html"); // Carrega o arquivo html para a janela criada
 
-  newWindow.webContents.openDevTools(); // Habilita o dev tolls
-});
-
-var Firebird = require('node-firebird');
-
-const options = {
-    host: '127.0.0.1',
-    port: 3050,
-    database: 'C:/BD/DB.FDB',
-    user: 'SYSDBA',
-    password: 'masterkey',
-    lowercase_keys: false, // set to true to lowercase keys
-    role: null,            // default
-    pageSize: 4096         // default when creating database
-}
-
-Firebird.attach(options, function(err, db) {
-    if (err)
-        throw err;
-
-    db.query('SELECT * FROM USUARIOS', function(err, result) {
-        console.log('Results: ', result);
-        db.detach();
-    });
-
+  // newWindow.webContents.openDevTools(); // Habilita o dev tolls
 });
