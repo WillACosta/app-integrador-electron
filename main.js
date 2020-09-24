202d8e22e391f344e21b0189e31b30ae100a13d2
William Amaral <williamamaral521@gmail.com>
2020-09-24 12:42:21 -0300

    Adição do Photon e criação das views do CRUD

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const { app, BrowserWindow } = require("electron");
 
+require('electron-reload')(__dirname);
+
 app.whenReady().then(() => {
-  const newWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    // titleBarStyle: 'hidden',
+    // alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
-  newWindow.loadFile("index.html"); // Carrega o arquivo html para a janela criada
+  mainWindow.setMenu(null);
+  mainWindow.webContents.openDevTools(); // Habilita o dev tolls
 
-  // newWindow.webContents.openDevTools(); // Habilita o dev tolls
+  mainWindow.loadFile("src/index.html"); // Carrega o arquivo html para a janela criada
+  mainWindow.loadUrl(`file://${__dirname}/index.html`); //Electron live-reload
+
 });
