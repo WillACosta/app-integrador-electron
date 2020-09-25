e65a8cc355e13dbad66ccfd0a9c03e6642564b66
William Amaral <williamamaral521@gmail.com>
2020-09-25 09:11:48 -0300

    Finalização listagem de usuários e scripts de build

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # app-integrador-electron [![Build Status](https://travis-ci.org/WillACosta/app-integrador-electron.svg?branch=master)](https://travis-ci.org/WillACosta/app-integrador-electron)
 
 Aplicação electron teste de integração com Firebird Database
+
+# Executar e empacotar
+
+- Em ambiente de desenvolvimento use `npm install` par instalar as dependências.
+- Para executar a aplicação use `npm run start`.
+
+- Criar pacotes para as plataformas
+
+  - Windows: `npm run package-win`.
+  - Linux: `npm run package-linux`.
+  - Mac: `npm run package-mac`.
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow } = require("electron");
+const {
+  app,
+  BrowserWindow
+} = require("electron");
 
 require('electron-reload')(__dirname);
 
@@ -6,15 +9,13 @@ app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // titleBarStyle: 'hidden',
-    // alwaysOnTop: true,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true
     },
   });
 
   mainWindow.setMenu(null);
-  mainWindow.webContents.openDevTools(); // Habilita o dev tolls
+  // mainWindow.webContents.openDevTools(); // Habilita o dev tolls
 
   mainWindow.loadFile("src/index.html"); // Carrega o arquivo html para a janela criada
   mainWindow.loadUrl(`file://${__dirname}/index.html`); //Electron live-reload
